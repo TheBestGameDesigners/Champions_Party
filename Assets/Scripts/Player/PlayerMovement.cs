b424f9bbab4a88bd36d07ffc41e853e6fc81841d
sergio semedi barranco <sergio.semedi@gmail.com>
2016-05-18 18:38:34 +0200

    migración del proyecto

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PlayerMovement : MonoBehaviour {
+	
+	
+	public float speed = 6f;
+
+
+    /* reworking */
+    Vector2 movement; //guarda la direccion del jugador
+    private Rigidbody2D body;
+
+
+    void Awake(){
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+
+        // Rotate player
+        var objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - objectPos;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+    }
+	
+
+
+  
+    void Move(float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(h, v);
+
+
+        Vector3 pos = transform.position;
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+        transform.position = pos;
+      
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other){
+        body.AddForce((-1)*movement * (2)*speed, ForceMode2D.Impulse);            
+    }
+
+  
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        body.velocity = Vector2.zero;
+
+    }
+}
---
+++ Assets/Scripts/Utils/cameraMove.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMove : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 cameraPos;
+    private Vector3 oldPosition;
+    private float height;
+    private float width;
+    private float borderLeft;
+    private float borderRigth;
+    private float borderUp;
+    private float borderDown;
+
+    void Start()
+    {
+        // transform.position = player.transform.position;
+        Camera cam = Camera.main;
+        height = 2f * cam.orthographicSize;
+        width = height * cam.aspect;
+        resetBorders();
+        oldPosition = player.transform.position;
+    }
+
+    void resetBorders(){
+        cameraPos = transform.position;
+        borderLeft = cameraPos.x - width / 2;
+        borderRigth = cameraPos.x + width / 2;
+        borderUp = cameraPos.y - height / 2;
+        borderDown = cameraPos.y + height / 2;
+    }
+
+    void FixedUpdate ()
+    {
+        Vector3 v3 = player.transform.position;
+
+        if (v3.x > borderRigth)
+        {
+            moveRight(v3);
+        }
+        if (v3.x < borderLeft)
+        {
+            moveLeft(v3);
+        }
+        if (v3.y > borderDown)
+        {
+            moveDown(v3);
+        }
+        if (v3.y < borderUp)
+        {
+            moveUp(v3);
+        }
+        resetBorders();
+    }
+
+    void moveRight(Vector3 v3)
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = transform.position.x + borderRigth - v3.x;
+        transform.position = newPos;
+    }
+
+    void moveLeft(Vector3 v3)
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = transform.position.x - borderLeft - v3.x;
+        transform.position = newPos;
+    }
+
+    void moveDown(Vector3 v3)
+    {
+        Vector3 newPos = transform.position;
+        newPos.y = transform.position.y + (borderDown - v3.y);
+        transform.position = newPos;
+    }
+
+    void moveUp(Vector3 v3)
+    {
+        Vector3 newPos = transform.position;
+        newPos.y = transform.position.y - borderUp - v3.y;
+        transform.position = newPos;
+    }
+}
