ee7d953d8741d80922fcf0c58d7e39c45ab44224
juanmont <juanmont@ucm.es>
2016-05-20 17:07:38 +0200

    HUD y vida de player

---
+++ Assets/Scripts/Enemy/EnemyAtack.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAtack : MonoBehaviour {
+
+    public float timeAttacks = 0.5f;
+    public int damage = 20;
+    public PlayerHealth p;
+    public GameObject player;
+    bool playerInRange = false;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer >= timeAttacks && playerInRange)
+        {
+            applyDamage();
+        }
+	}
+
+    void applyDamage()
+    {
+        timer = 0f;
+
+        if (p.currentHealth > 0)
+            p.takeDamage(damage);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+	// Use this for initialization
+	void Start () {
+        currentHealth = startHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0)
+            death();
+    }
+
+    void death()
+    {
+        Destroy(gameObject);
+    }
+}
