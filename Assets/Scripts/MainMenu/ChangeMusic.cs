dc40bb4733ce3ca724d0b7e9a592580d3610658a
alvasenj <alvasenj@ucm.es>
2016-05-21 12:11:25 +0200

    menu

---
+++ Assets/Scripts/MainMenu/ChangeMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+    public AudioClip levelMusic;
+
+    private AudioSource source;
+
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded () {
+        source.clip = levelMusic;
+        source.Play();
+	}
+}
