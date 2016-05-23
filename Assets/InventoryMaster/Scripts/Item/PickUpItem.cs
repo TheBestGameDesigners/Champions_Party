15066def36dabfece60974641bb13a4f8a37352c
sergio semedi barranco <sergio.semedi@gmail.com>
2016-05-23 20:01:55 +0200

    puedes coger objetos, hay bugs

--- Assets/InventoryMaster/Scripts/Item/PickUpItem.cs
+++ Assets/InventoryMaster/Scripts/Item/PickUpItem.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+
+
 public class PickUpItem : MonoBehaviour
 {
     public Item item;
@@ -7,8 +10,12 @@ public class PickUpItem : MonoBehaviour
     private GameObject _player;
     // Use this for initialization
 
+
+    private bool actionPressed;
+
     void Start()
     {
+        actionPressed = false;
         _player = GameObject.FindGameObjectWithTag("Player");
         if (_player != null)
             _inventory = _player.GetComponent<PlayerInventory>().inventory.GetComponent<Inventory>();
@@ -17,25 +24,36 @@ public class PickUpItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_inventory != null && Input.GetKeyDown(KeyCode.E))
+        /* actionPressed = (Input.GetKeyDown(KeyCode.E));
+         if (actionPressed)
+             Debug.Log("actionPressed Down");*/
+        
+          
+
+    }
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+
+        if (Input.GetKey(KeyCode.E)) { 
+
+        
+        bool check = _inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
+        if (check)
+            Destroy(this.gameObject);
+        else if (_inventory.ItemsInInventory.Count < (_inventory.width * _inventory.height))
         {
-            float distance = Vector3.Distance(this.gameObject.transform.position, _player.transform.position);
-
-            if (distance <= 3)
-            {
-                bool check = _inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
-                if (check)
-                    Destroy(this.gameObject);
-                else if (_inventory.ItemsInInventory.Count < (_inventory.width * _inventory.height))
-                {
-                    _inventory.addItemToInventory(item.itemID, item.itemValue);
-                    _inventory.updateItemList();
-                    _inventory.stackableSettings();
-                    Destroy(this.gameObject);
-                }
-
-            }
+            _inventory.addItemToInventory(item.itemID, item.itemValue);
+            _inventory.updateItemList();
+            _inventory.stackableSettings();
+            Destroy(this.gameObject);
         }
+        }
+
+
+
     }
 
 }
