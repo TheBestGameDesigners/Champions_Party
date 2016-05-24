0d3659f24f07ad87910ea6658d0ca9a3f12a4e7a
juanmont <juanmont@ucm.es>
2016-05-24 20:01:12 +0200

    copa

---
+++ Assets/InventoryMaster/Scripts/moveTrophy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTrophy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(Vector3.right * Time.deltaTime * 100);
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+}
