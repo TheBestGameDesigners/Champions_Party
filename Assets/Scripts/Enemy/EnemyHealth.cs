4104943760093e7ee7f0c870eadbcf016ba3dd18
alvasenj <alvasenj@ucm.es>
2016-05-24 16:25:43 +0200

    enemy con vida

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int startHealth = 100;
+    public int currentHealth;
+
+    // Use this for initialization
+
+    public Color[] originalColors;
+    public Material[] materials;// All the Materials of this & its child
+    public int remainingDamageFrames = 0;
+    public int showDamageForFrames = 5;
+    private Color originalColor;
+
+    void awake()
+    {
+        originalColor = GetComponent<SpriteRenderer>().color;   
+    }
+    void Start()
+    {
+        currentHealth = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingDamageFrames > 0)
+        {
+            remainingDamageFrames--;
+            if (remainingDamageFrames == 0)
+            {
+                GetComponent<SpriteRenderer>().color = originalColor; 
+            }
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+            death();
+    }
+
+    void death()
+    { 
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {        
+        if (other.CompareTag("bullet"))
+        {
+            takeDamage(25);
+            ShowDamage();      
+        }
+    }
+
+    void ShowDamage()
+    {
+        remainingDamageFrames = 5;
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
