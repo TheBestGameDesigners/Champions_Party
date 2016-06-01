7a73dde195b74a94d4f92929c202abc36eba7647
sergio semedi barranco <sergio.semedi@gmail.com>
2016-06-01 18:43:20 +0200

    empezado el sistema de loot

---
+++ Assets/Scripts/Loot/WeaponLoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponLoot : MonoBehaviour
+{
+
+    public int amountOfLoot = 10;
+    static ItemDataBaseList inventoryItemList;
+
+    int counter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+
+       
+
+            int randomNumber = Random.Range(1, inventoryItemList.itemList.Count - 1);
+
+
+          
+                GameObject randomLootItem = (GameObject)Instantiate(inventoryItemList.itemList[0].itemModel);
+                PickUpItem item = randomLootItem.AddComponent<PickUpItem>();
+                item.item = inventoryItemList.itemList[0];
+
+          
+                
+
+    }
+
+}
