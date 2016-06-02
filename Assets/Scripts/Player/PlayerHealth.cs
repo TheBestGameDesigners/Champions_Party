5e6091493b325f5549c86498b9a3c49eeb6fd1a2
alvasenj <alvasenj@ucm.es>
2016-06-02 13:59:29 +0200

    camera bounds y anim player

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,12 +4,19 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
+    private int cont;
     public int startHealth = 100;
     public int currentHealth;
     Slider healthSlider;
+    Animator anim;
 	// Use this for initialization
 
+    void Awake() {
+
+        anim = GetComponent<Animator>();
+    }
 	void Start () {
+        
         currentHealth = Manager.m.healthPlayer;
         //GameObject hud = GameObject.FindGameObjectsWithTag("HUD")[0];
         healthSlider = GameObject.FindGameObjectsWithTag("HUD")[0].transform.Find("Slider").GetComponent<Slider>();
@@ -19,8 +26,8 @@ public class PlayerHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        cont = anim.runtimeAnimatorController.animationClips.Length;
+    }
 
     public void takeDamage(int damage)
     {
@@ -28,11 +35,23 @@ public class PlayerHealth : MonoBehaviour {
         healthSlider.value = currentHealth;
         Manager.m.healthPlayer = currentHealth;
         if (currentHealth <= 0)
-            death();
+        {
+            if (anim.runtimeAnimatorController.animationClips.Length > 1)
+            {
+                float time = anim.runtimeAnimatorController.animationClips[3].length;
+                Debug.Log(time);
+
+                anim.SetBool("isDead", true);
+                Invoke("death", time);
+            }
+        }
     }
 
     void death()
     {
         Destroy(gameObject);
+        Destroy(anim);
+
+
     }
 }
