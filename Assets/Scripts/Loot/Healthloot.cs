17f2fa09b77357532455da80353e82f7f921eb80
Sergio <sergio.semedi@gmail.com>
2016-06-12 21:58:56 +0100

    añadidos todos los tipos de loot

---
+++ Assets/Scripts/Loot/Healthloot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healthloot : MonoBehaviour {
+
+    public int amountOfLoot = 5;
+
+    static ItemDataBaseList inventoryItemList;
+
+    int counter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+        StorageInventory loot = GetComponent<StorageInventory>();
+        int id = 0;
+
+        for (int i = 0; i < amountOfLoot; i++)
+        {
+
+
+            int dropChance = Random.Range(0, 100);
+
+
+            if (dropChance > 60)
+                continue;
+            else if (dropChance > 0 && dropChance < 10)
+                id = 6;
+            else if (dropChance > 10 && dropChance < 30)
+                id = 5;
+            else
+                id = 7;
+
+
+
+
+
+
+
+            Item item = inventoryItemList.getItemByID(id);
+            item.itemValue = 1;
+            loot.addItem(item);
+
+
+        }
+
+    }
+}
