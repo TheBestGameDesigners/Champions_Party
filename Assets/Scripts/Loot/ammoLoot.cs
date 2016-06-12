a167a1450ff917ff3e3fff2e9a0304cbfd5b7aef
Sergio <sergio.semedi@gmail.com>
2016-06-12 22:26:41 +0100

    arreglado caja de balas

---
+++ Assets/Scripts/Loot/ammoLoot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ammoLoot : MonoBehaviour
+{
+
+    public int amountOfLoot = 2;
+
+    static ItemDataBaseList inventoryItemList;
+
+    int counter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+        StorageInventory loot = GetComponent<StorageInventory>();
+      
+
+        for (int i = 0; i < amountOfLoot; i++)
+        {
+
+
+            int dropChance = Random.Range(0, 100);
+
+
+            if (dropChance > 30)
+                continue;
+            else {
+                Item item = inventoryItemList.getItemByID(4);
+                item.itemValue = 1;
+                loot.addItem(item);
+            }
+
+
+        }
+
+    }
+}
